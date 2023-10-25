7cf3fc3da4fc5a5ee9d2fd4daacd6bf4ee1c9a29
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-10-25 12:57:30 +0000

    Add constraint for positive amounts only

--- lesson_1/expense_project/schema.sql
+++ lesson_1/expense_project/schema.sql
@@ -1,6 +1,11 @@
+DROP TABLE expenses;
+
 CREATE TABLE expenses (
   id serial PRIMARY KEY,
   amount numeric(6,2) NOT NULL,
   memo text NOT NULL,
   created_on date NOT NULL
 );
+
+ALTER TABLE expenses
+ADD CONSTRAINT positive_amounts_only CHECK(amount > 0);
